436865faead227ef650e2cf7841d0c138f3c4430
projectmiluju <dnjsdyd0712@naver.com>
2025-10-15 20:15:33 +0900

    refactor: canvas feature 공개 API 정리

--- src/features/canvas/index.ts
+++ src/features/canvas/index.ts
@@ -1,2 +1,4 @@
 export { Canvas } from "./components/Canvas/Canvas";
 export { useCanvasStore } from "./stores/canvasStore";
+export { nodeTypes } from "./nodeTypes";
+export type { DocumentNodeData, ImageNodeData, NoteNodeData } from "./types";
