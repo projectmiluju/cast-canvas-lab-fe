c9d1b88479f62dae283dfda35792c1633f04c052
projectmiluju <dnjsdyd0712@naver.com>
2025-10-16 20:15:59 +0900

    test: 테스트 환경 설정 업데이트

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,19 @@
 import "@testing-library/jest-dom";
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+class DOMMatrixMock {
+  a = 1;
+  b = 0;
+  c = 0;
+  d = 1;
+  e = 0;
+  f = 0;
+}
+
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+vi.stubGlobal("DOMMatrix", DOMMatrixMock);
