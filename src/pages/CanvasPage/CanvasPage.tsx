7093a8ab20423a7aeefe932f010a4a9241bae4d6
projectmiluju <dnjsdyd0712@naver.com>
2025-10-20 21:26:19 +0900

    feat: CanvasPage 레이아웃 개선 및 Inspector 패널 통합

--- src/pages/CanvasPage/CanvasPage.tsx
+++ src/pages/CanvasPage/CanvasPage.tsx
@@ -1,8 +1,12 @@
 import { Canvas } from "../../features/canvas";
+import { Inspector } from "../../features/inspector";
 import styles from "./CanvasPage.module.scss";
 
 export const CanvasPage = () => (
   <main className={styles.page}>
-    <Canvas />
+    <div className={styles.canvasArea}>
+      <Canvas />
+    </div>
+    <Inspector />
   </main>
 );
