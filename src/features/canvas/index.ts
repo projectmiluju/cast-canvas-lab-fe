4008e1d3820ebaee8cef3e1cc9e403478a749a04
projectmiluju <dnjsdyd0712@naver.com>
2025-10-27 01:16:54 +0900

    feat: Canvas 컴포넌트에 edgeTypes 적용 및 내보내기 설정

--- src/features/canvas/index.ts
+++ src/features/canvas/index.ts
@@ -1,4 +1,5 @@
 export { Canvas } from "./components/Canvas/Canvas";
 export { useCanvasStore } from "./stores/canvasStore";
 export { nodeTypes } from "./nodeTypes";
+export { edgeTypes } from "./edgeTypes";
 export type { DocumentNodeData, ImageNodeData, NoteNodeData } from "./types";
